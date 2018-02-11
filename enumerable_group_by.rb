4c5adf2313dc8bd08f3e7fc276c90ae6466cede7
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-02-11 10:29:15 -0600

    update group_by

--- enumerable_group_by.rb
+++ enumerable_group_by.rb
@@ -2,10 +2,8 @@
 marks = {"Ramesh":23, "Vivek":40, "Harsh":88, "Mohammad":60}
 
 def group_by_marks(marks, pass_marks)
-    status = marks.group_by{|name, grade| grade >= pass_marks}
-    status["Failed"] = status.delete(false)
-    status["Passed"] = status.delete(true)
-    return status
+    return marks.group_by {|name, grade| grade < pass_marks ? "Failed" : "Passed" }
 end
+
 
 p group_by_marks(marks, 80)
